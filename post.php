7ab3c8b82fa52e9aeb34cc2e8812a760d6396097
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-03-26 23:30:56 +0700

    Add Edit Action

---
+++ post.php
@@ -0,0 +1,9 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Request-With");
+    header("Content-Type: application/json; charset=utf-8");
+
+?>
