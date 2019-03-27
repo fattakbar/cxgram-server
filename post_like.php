adfb9a387e4414c0f300ddfc3f5dd7707f507858
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-03-27 10:30:11 +0700

    Add post_like.php Files

---
+++ post_like.php
@@ -0,0 +1,13 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Request-With");
+    header("Content-Type: application/json; charset=utf-8");
+
+    include "library/config.php";
+    include "library/function_validation.php";
+    include "library/function_date.php";
+
+?>
