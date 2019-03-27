12a0ecee2525520b3b25da06b51326edc69f0180
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-03-27 17:36:48 +0700

    post_like.php Done

--- post_like.php
+++ post_like.php
@@ -10,4 +10,33 @@
     include "library/function_validation.php";
     include "library/function_date.php";
 
+    $post = json_decode(file_get_contents('php://input'), true);
+
+    if(member_valid($post['username'], $post['password'])){
+        $jml_like = mysqli_num_rows(mysqli_query($mysqli, "SELECT * FROM post_like WHERE id_post='$post[idpost]' AND id_member='$post[member]'"));
+
+        if($jml_like == 0){
+            $query = mysqli_query($mysqli, "INSERT INTO post_like SET
+                    id_member   = '$post[member]',
+                    id_post     = '$post[idpost]'
+            ");
+
+            if($query) mysqli_query($mysqli, "INSERT INTO notification SET
+                    id_member       = '$post[member]',
+                    message         = 'menyukai kiriman anda',
+                    member_target   = '$post[target]',
+                    new             = 1,
+                    id_post         = '$post[idpost]'
+            ");
+
+            $like = 1;
+        }else{
+            $query = mysqli_query($mysqli, "DELETE FROM post_like WHERE id_member='$post[member]' AND id_post='$post[idpost]'");
+            $like = 0;
+        }
+        if($query) $result = json_encode(array('success'=>true, 'result'=>$like));
+        else $result = json_encode(array('success'=>false));
+        echo $result;
+    }
+
 ?>
