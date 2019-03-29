b27ae81a27a044a374e30c7afc42da3c32286d9e
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-03-29 23:44:37 +0700

    Add Read Action

--- comment.php
+++ comment.php
@@ -12,4 +12,24 @@
 
     $post = json_decode(file_get_contents('php://input'), true);
 
+    if(member_valid($post['username'], $post['password'])){
+        if($post['aksi'] == "tampil"){
+            $data = array();
+            $query = mysqli_query($mysqli, "SELECT * FROM comment LEFT JOIN member ON comment.id_member=member.id_member WHERE id_post='$post[idpost]' ORDER BY comment.id_comment");
+
+            while($row = mysqli_fetch_array($query)){
+                $data[] = array(
+                    'id'        => $row['id_comment'],
+                    'id_member' => $row['id_member'],
+                    'nama'      => $row['name'],
+                    'komentar'  => $row['comment'],
+                    'tanggal'   => tgl_indonesia($row['created_at'])
+                );
+            }
+            if($query) $result = json_encode(array('success'=>true, 'result'=>$data));
+            else $result = json_encode(array('success'=>false));
+            echo $result;
+        }
+    }
+
 ?>
