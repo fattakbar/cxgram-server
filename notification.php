0be3ca538a962435db0b38d8518088c8d748eaa9
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-04-03 23:42:18 +0700

    Update notification.php Files

--- notification.php
+++ notification.php
@@ -11,4 +11,23 @@
 
     $post = json_decode(file_get_contents('php://input'), true);
 
+    if(member_valid($post['username'], $post['password'])){
+        $data = array();
+        $query = mysqli_query($mysqli, "SELECT * FROM notification LEFT JOIN member ON notification.id_member=member.id_member WHERE member_target='$post[member]' ORDER BY notification DESC");
+
+        while($row = mysqli_fetch_array($query)){
+            $data[] = array(
+                'id'            => $row['id_notification'],
+                'id_member'     => $row['id_member'],
+                'nama'          => $row['name'],
+                'notifikasi'    => $row['message'],
+                'baru'          => $row['new'],
+                'target'        => $row['member_target'],
+                'idpost'        => $row['id_post'],
+                'tanggal'       => tgl_indonesia($row['created_at'])
+            );
+
+        }
+    }
+
 ?>
